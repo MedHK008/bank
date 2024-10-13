babb71de239c93b86ac80655d1f79341b1f58ad4
HERAK Mohammed <mohammedherak37@gmail.Com>
2024-10-13 11:00:47 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,33 +1,73 @@
 #include "Account.h"
 #include "Bank.h"
+#include <iostream>
 
 using namespace std;
-// Example usage of the system
+
+void displayMenu() {
+    cout << "1. Deposit" << endl;
+    cout << "2. Withdraw" << endl;
+    cout << "3. Transfer" << endl;
+    cout << "4. Show Account" << endl;
+    cout << "5. Save Accounts" << endl;
+    cout << "6. Exit" << endl;
+    cout << "Enter your choice: ";
+}
+
 int main() {
     Bank bank;
-
-    // Add accounts
-    auto* acc1 = new Account(1, "John Doe","235641897511168894615073", 1000.0);
-    auto* acc2 = new SavingsAccount(2, "Jane Smith","345486455217843541684561", 2000.0, 0.05);
-
-
-    acc1->showAccount();
-    acc2->showAccount();
-
-    bank.addAccount(acc1);
-    bank.addAccount(acc2);
-
-    // Perform transactions
-    acc1->deposit(500);
-    acc1->showAccount();
-    acc2->withdraw(300);
-    acc2->showAccount();
-    acc1->transfer(*acc2, 200);
-    acc1->showAccount();
-    acc2->showAccount();
-
-    // Save accounts to file
-    bank.saveAllAccounts("accounts.txt");
-
-    return 0;
+    bank.loadAllAccounts("./accounts.txt");
+    int choice = 0;
+    int accountId;
+    double amount;
+    int targetAccountId;
+    while (true) {
+        cout << "Enter your account ID: ";
+        cin >> accountId;
+        Account* account = bank.findAccount(accountId);
+        if (account == nullptr) {
+            cout << "Account not found!" << endl;
+            continue;
+        }
+        while(choice != 6) {
+            displayMenu();
+            cin >> choice;
+            switch (choice) {
+                case 1:
+                    cout << "Enter amount to deposit: ";
+                    cin >> amount;
+                    account->deposit(amount);
+                break;
+                case 2:
+                    cout << "Enter amount to withdraw: ";
+                    cin >> amount;
+                    account->withdraw(amount);
+                    break;
+                case 3:
+                    cout << "Enter target account ID: ";
+                    cin >> targetAccountId;
+                    cout << "Enter amount to transfer: ";
+                    cin >> amount;
+                    if (Account* targetAccount = bank.findAccount(targetAccountId); targetAccount != nullptr) {
+                        account->transfer(*targetAccount, amount);
+                    } else {
+                        cout << "Target account not found!" << endl;
+                    }
+                    break;
+                case 4:
+                    account->showAccount();
+                break;
+                case 5:
+                    bank.saveAllAccounts("accounts.txt");
+                break;
+                case 6:
+                    return 0;
+                default:
+                    cout << "Invalid choice!" << endl;
+            }
+        }
+        cout << "Do you want to continue?(1 for y/0 fo n):";
+        cin >> choice;
+        if (choice == 1) return 0;
+    }
 }
